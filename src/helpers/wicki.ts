a79f28ea624526262083d6c6a447016e1da2df6b
mdsharifulislam-r <masom3456@gmail.com>
2025-11-07 12:29:36 +0600

    kjk

--- src/helpers/wicki.ts
+++ src/helpers/wicki.ts
@@ -178,7 +178,7 @@ export const savedLocationsInDBParrelal = async (locations: LocationInfo[],place
             formattedAddress: item.title+","+place,
             imageUrl: [],
             summary: '',
-            type:"" ,
+            type: 'Other',
             city: '',
             state: '',
             country: '',
