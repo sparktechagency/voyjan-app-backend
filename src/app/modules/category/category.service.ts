953e87b5da67bbc5d40c65ea2ba60d5e4727c321
mdsharifulislam-r <masom3456@gmail.com>
2025-11-11 09:42:42 +0600

    ewew

--- src/app/modules/category/category.service.ts
+++ src/app/modules/category/category.service.ts
@@ -1,3 +1,4 @@
+import { redisClient } from "../../../config/redis.client";
 import { RedisHelper } from "../../../helpers/redisHelper";
 import { ICategory } from "./category.interface";
 import { Category } from "./category.model";
@@ -5,6 +6,7 @@ import { Category } from "./category.model";
 const createCategoryIntoDB = async (data:ICategory) => {
     const category = await Category.create(data);
     await RedisHelper.keyDelete('category');
+    await redisClient.del('category');
     return category;
 };
 
@@ -24,12 +26,14 @@ const updateCategoryIntoDB = async (id: string, data: ICategory) => {
         new: true,
     });
     await RedisHelper.keyDelete('category');
+    await redisClient.del('category');
     return category;
 };
 
 const deleteCategoryFromDB = async (id: string) => {
     const category = await Category.findOneAndDelete({ _id: id });
     await RedisHelper.keyDelete('category');
+    await redisClient.del('category');
     return category;
 };
 
