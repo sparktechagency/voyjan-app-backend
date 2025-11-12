4db32ceeac8248a3a145ff8f6805c68dd8cb1b84
mdsharifulislam-r <masom3456@gmail.com>
2025-11-12 09:23:39 +0600

    ere

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -3,7 +3,7 @@ import { Address } from "../app/modules/address/address.model";
 import { addDetailsInExistingAddress, addTypeInExistingAddress } from "../helpers/wicki";
 import { AddressService } from "../app/modules/address/address.service";
 
-export async function startWorker() {
+export function startWorker() {
     // cronJob.schedule("*/5 * * * *",async () => {
     //     try {
     //         console.log('Cron Job Runned');
@@ -20,58 +20,30 @@ export async function startWorker() {
     //     }
     // });
 
-    const address = await Address.find()
-    if (address.length > 0) {
-      for (const data of address) {
-        //remove all = from summury field
-        const summary = data.summary?.replace(/=/g, '');
-        const long_descreption = data.long_descreption?.replace(/=/g, '');
-        let diff_lang = {...data?.diff_lang as any}
-       Object.keys(data?.diff_lang!).map((key) =>{
-        diff_lang[key]={
-          title:data?.diff_lang![key as any].title?.replace(/=/g, ''),
-          translateText:data?.diff_lang![key as any].translateText?.replace(/=/g, ''),
-          translateLong:data?.diff_lang![key as any].translateLong?.replace(/=/g, ''),
-          address:data?.diff_lang![key as any].address?.replace(/=/g, ''),
-        }
-       });
-        await Address.findOneAndUpdate({ _id: data._id }, { summary: summary, long_descreption: long_descreption, diff_lang: diff_lang }, { new: true });
-        console.log(`Cron Job Runned for ${data._id}`);
+    // run every 15 seconds
+    cronJob.schedule("*/15 * * * * *", async () => {
+  try {
+    // console.log("Cron Job Runned");
+
+    const finishedData = await Address.find({ summary: "" }).limit(1).lean();
+    console.log(finishedData);
+
+    if (finishedData.length > 0) {
+      for (const data of finishedData) {
+        await AddressService.singleAaddressFromDB(data._id.toString());
       }
     }
 
-    // run every 15 seconds
-//     cronJob.schedule("*/1 * * * * *", async () => {
-//   try {
-//     // console.log("Cron Job Runned");
+    // await addDetailsInExistingAddress(finishedData as any);
+    // const otherTypes = await Address.findOne({type:{$in:['','Other']}})
+    // if(otherTypes){
+    //   await addTypeInExistingAddress(otherTypes as any)
+    // }
 
-//     // const finishedData = await Address.find({ diff_lang:""}).limit(1).lean();
-//     // console.log(finishedData);
+    console.log("Cron Job Runned");
 
-//     // if (finishedData.length > 0) {
-//     //   for (const data of finishedData) {
-//     //     await AddressService.singleAaddressFromDB(data._id.toString());
-//     //   }
-//     // }
-
-//     const address = await Address.find()
-//     if (address.length > 0) {
-//       for (const data of address) {
-//         await AddressService.singleAaddressFromDB(data._id.toString());
-//       }
-//     }
-
-    
-
-//     // await addDetailsInExistingAddress(finishedData as any);
-//     // const otherTypes = await Address.findOne({type:{$in:['','Other']}})
-//     // if(otherTypes){
-//     //   await addTypeInExistingAddress(otherTypes as any)
-//     // }
-
-//     console.log("Cron Job Runned");
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
+  } catch (error) {
+    console.log(error);
+  }
+});
 }
