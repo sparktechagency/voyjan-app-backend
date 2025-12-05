cc31bae715f7136a05783ddadd7ee7cde65eb57d
md shariful Islam <eng.mdshariful.islam.7@gmail.com>
2025-12-05 17:03:29 +0600

    er

--- src/app/modules/address/address.service.ts
+++ src/app/modules/address/address.service.ts
@@ -261,7 +261,7 @@ const singleAaddressFromDB = async (addressId: string,lang:string='English') => 
 
   const isExist = await Category.findOne({ name: address.diff_lang?.[lang]?.type });
   if (isExist) {
-    const translateCategory = await singleTextTranslation(address.name,lang);
+    const translateCategory = await singleTextTranslation(address.diff_lang?.[lang]?.type!,lang);
     singleCategoryChangeAndSave(address._id.toString(),translateCategory,lang);
     address.diff_lang = {
       ...address.diff_lang,
