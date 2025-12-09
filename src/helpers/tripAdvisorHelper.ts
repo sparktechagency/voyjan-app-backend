f15abf98ff8dd3a79833e377469416fd22272eb2
md shariful Islam <eng.mdshariful.islam.7@gmail.com>
2025-12-09 11:18:48 +0600

    rem: unable the save photo in local using the trip advisor api

--- src/helpers/tripAdvisorHelper.ts
+++ src/helpers/tripAdvisorHelper.ts
@@ -7,7 +7,7 @@ const getLocationDetails = async (address: string,lang:string='en',addressId:str
         const response = await axios.get(`https://api.content.tripadvisor.com/api/v1/location/search?key=${config.tripAdvisor.key}&searchQuery=${address}`);
         const locationId = response.data?.data?.[0]?.location_id;
         const locationDetails = await axios.get(`https://api.content.tripadvisor.com/api/v1/location/${locationId}/details?language=${lang}&currency=USD&key=${config.tripAdvisor.key}`);
-        savePhotosInAddress(addressId,locationId)
+        // savePhotosInAddress(addressId,locationId)
         return locationDetails.data?.web_url
     } catch (error) {
         return ''
